e4e79d6c207bad26519fb86f352e85019d7a7cac
jacephillips7 <jacedelynn@gmail.com>
2024-12-05 16:21:16 -0600

    this is not working and I want to die

--- day4/main.go
+++ day4/main.go
@@ -21,18 +21,23 @@ func main() {
 	for y, y_line := range full_array {
 		for x := range y_line {
 			//this is for part 1
-			if checkSpace(full_array, "X", x, y) { //start by looking for the X
-				//we found the X, now get the directions of M
-				directions := searchFor(full_array, "M", x, y)
-				for _, d := range directions {
-					x_direction := d[0]
-					y_direction := d[1]
-					if matchString(full_array, "XMAS", x, y, x_direction, y_direction) {
-						totalCount++
-					}
-				}
-			}
+			// if checkSpace(full_array, "X", x, y) { //start by looking for the X
+			// 	//we found the X, now get the directions of M
+			// 	directions := searchFor(full_array, "M", x, y)
+			// 	for _, d := range directions {
+			// 		x_direction := d[0]
+			// 		y_direction := d[1]
+			// 		if matchString(full_array, "XMAS", x, y, x_direction, y_direction) {
+			// 			totalCount++
+			// 		}
+			// 	}
+			// }
 			//this is for part 2
+			if checkSpace(full_array, "A", x, y) {
+				//a is found, check if we can make an X with 'mas'
+				found := checkX_MAS(full_array, x, y)
+				part2Count += found
+			}
 		}
 	}
 	fmt.Print("Part 1 total: ")
@@ -113,4 +118,64 @@ func checkSpace(arr [][]string, search string, x int, y int) bool {
 		return false
 	}
 	return arr[y][x] == search
+}
+
+func getInvers(arr []int) []int {
+	inverted := make([]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		inverted[i] = arr[i] * -1
+	}
+	return inverted
+}
+
+func checkX_MAS(arr [][]string, x int, y int) int {
+	//fuck it we ball
+	//I'm going to convert all characters around the A at this location to be a numeric value
+	// M = 10
+	// S = 1
+	// else = 0
+	// so in a 3x3 grid, A being the center like so
+	//		MSM
+	//		MAS
+	//		SMS
+	// we have 3 outcomes,
+	// a '+' shape, a 'x' shape, and both
+	// so when we check each line in the shape for a total of 11
+	total := 0
+	x_legs := [][]int{
+		{-1, 1},
+		{1, 1},
+	}
+	plus_legs := [][]int{
+		{0, 1},
+		{1, 0},
+	}
+	if check_shape(arr, x, y, x_legs) {
+		total++
+	}
+	if check_shape(arr, x, y, plus_legs) {
+		total++
+	}
+	return total
+}
+func check_shape(arr [][]string, x int, y int, legs [][]int) bool {
+	passing := 0
+	for _, leg := range legs {
+		invert_leg := getInvers(leg)
+		calc := getValue(arr, x+leg[0], y+leg[1]) + getValue(arr, x+invert_leg[0], y+invert_leg[1])
+		if calc != 11 {
+			return false
+		}
+		passing++
+	}
+	return passing == 2
+}
+func getValue(arr [][]string, x int, y int) int {
+	if checkSpace(arr, "M", x, y) {
+		return 10
+	}
+	if checkSpace(arr, "S", x, y) {
+		return 1
+	}
+	return 0
 }
