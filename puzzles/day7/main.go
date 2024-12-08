cd117c75a38fd93c169dfe98af0e0f1dc1c4fff2
jacephillips7 <jacedelynn@gmail.com>
2024-12-07 23:21:10 -0600

    part1

---
+++ puzzles/day7/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	read_file "aoc2024/library"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file := read_file.ReadFile("./input.txt")
+	problems := read_file.SplitOnLine(file)
+	var total uint64
+	total = 0
+	for _, p := range problems {
+		cleanString := strings.ReplaceAll(p, ":", "")
+		parts := strings.Split(cleanString, " ")
+		s, n := arrPop(parts)
+		solution, _ := strconv.Atoi(s)
+		numbs := make([]uint64, len(n))
+		for i, v := range n {
+			x, _ := strconv.Atoi(v)
+			numbs[i] = uint64(x)
+		}
+		can, t := canSolve(uint64(solution), numbs)
+		if can {
+			total += t
+		}
+	}
+	fmt.Println(total)
+}
+
+func canSolve(solution uint64, numbs []uint64) (bool, uint64) {
+	//fill the operators
+	operators := createAllPossible(len(numbs))
+	return runTest(numbs, operators, solution)
+}
+
+func createAllPossible(n int) [][]string {
+	result := [][]string{}
+	arr := make([]string, n)
+
+	var generate func(index int)
+	generate = func(index int) {
+		if index == n {
+			// Make a copy of the current combination and add it to result
+			combination := make([]string, n)
+			copy(combination, arr)
+			result = append(result, combination)
+			return
+		}
+
+		// Try "+" at the current position and recurse
+		arr[index] = "+"
+		generate(index + 1)
+
+		// Try "*" at the current position and recurse
+		arr[index] = "*"
+		generate(index + 1)
+	}
+
+	generate(0) // Start recursion
+	return result
+}
+
+func runTest(numbs []uint64, possible_operators [][]string, solution uint64) (bool, uint64) {
+	j := 0
+	for j < len(possible_operators) {
+		operators := possible_operators[j]
+		test_solution := numbs[0]
+		for i := 1; i < len(numbs); i++ {
+			if operators[i-1] == "+" {
+				test_solution += numbs[i]
+			} else {
+				test_solution = test_solution * numbs[i]
+			}
+		}
+		if test_solution == solution {
+			return true, test_solution
+		}
+		j++
+	}
+
+	return false, 0
+}
+
+func arrPop(arr []string) (string, []string) {
+	popper := arr[0]
+	return popper, arr[1:]
+}
